6675139e4012099107c43dfd2cdacae1f8b81dde
Fábio Pereira <fabio.pereira@outlook.com>
2024-01-13 20:50:46 -0300

    Finaliza endpoints,  swagger e log

---
+++ Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace movies_api_pipeline_actions_azure_ci.Configuration
+{
+    public static class SwaggerConfiguration
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesAPI", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+    }
+}
--- Dtos/CreateMovieDto.cs
+++ Dtos/CreateMovieDto.cs
@@ -2,22 +2,9 @@
 
 namespace movies_api_pipeline_actions_azure_ci.Dtos
 {
-    public class CreateMovieDto
+    public class CreateMovieDto : MovieDto
     {
-        [Required(ErrorMessage = "Movie Title is required")]
-        [StringLength(50, ErrorMessage = "Movie Title must not exceed 50 characters")]
-        [Display(Name = "Title")]
-        public string Title { get; set; }
-
-
-        [Required(ErrorMessage = "Movie Genre is required")]
-        [StringLength(30, ErrorMessage = "Movie Genre must not exceed 30 characters")]
-        [Display(Name = "Genre")] public string Genre { get; set; }
-
-        [Required(ErrorMessage = "Movie duration is required")]
-        [Range(1, 300, ErrorMessage = "Movie Duration must be between 1 and 300 minutes")]
-        [Display(Name = "Duration (minutes)")]
-        public int Duration { get; set; }
+       
     }
 }
 
