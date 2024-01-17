e5ae82c1db701761de79c224cd95faf76134d2e7
Fábio Pereira <fabio.pereira@outlook.com>
2024-01-17 20:13:17 -0300

    Add version to swagger

--- Configuration/SwaggerConfiguration.cs
+++ Configuration/SwaggerConfiguration.cs
@@ -7,9 +7,12 @@ namespace movies_api_pipeline_actions_azure_ci.Configuration
     {
         public static void AddSwaggerConfiguration(this IServiceCollection services)
         {
+            
+            string version = Environment.GetEnvironmentVariable("APP_VERSION") ?? "Development";
+            
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesAPI", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesAPI", Version = version });
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
